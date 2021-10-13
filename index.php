297031fb5f4a092e49afd3c65b6519ca4629e8c5
Matteo92i <matteo.iovane@gmail.com>
2021-10-13 15:41:39 +0200

    modifiche css

--- index.php
+++ index.php
@@ -42,15 +42,39 @@ $domandeRisposte = [
 
 ?>
 
-<ol>
-    <?php foreach($domandeRisposte as $domandaRisposta) { ?>
-        <li> 
-          <?php echo $domandaRisposta ['domandaGoogle']; ?> 
-          
-          <?php  echo $domandaRisposta ['rispostaGoogle']; ?> 
-        </li>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
 
-    <?php } ?>  
-    
+  <ol>
+      <?php  foreach($domandeRisposte as $domandaRisposta) { ?>
+          <li> 
+            <div class="domanda">
+              <?php echo $domandaRisposta ['domandaGoogle']; ?> 
+            </div>
+            <div class="risposta">
+              <?php  echo $domandaRisposta ['rispostaGoogle']; ?>
+            </div>
+          </li>
+       <?php } ?>  
+    </ol>
+  </body>
+  </html>
 
-</ol>
+            
+            
+             
+  
+      
+  
+
+
+
+
